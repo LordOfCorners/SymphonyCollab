64d4d9595e85d8d95279365d0af01d3e06824d69
sheva29 <design@sanchezmauricio.com>
2013-10-05 17:14:06 -0400

    Adding my own class

---
+++ nwsPrototype1/src/Dancer.cpp
@@ -0,0 +1,34 @@
+//
+//  Dancer.cpp
+//  nwsPrototype1
+//
+//  Created by Mauricio Sanchez Duque on 10/5/13.
+//
+//
+
+#include "Dancer.h"
+
+
+
+Dancer::Dancer(){
+    
+}
+
+
+
+
+void Dancer::draw(){
+    
+    
+}
+
+
+
+
+void Dancer::update(){
+    
+    
+    
+    
+    
+}
---
+++ nwsPrototype1/src/Dancer.h
@@ -0,0 +1,32 @@
+//
+//  Dancer.h
+//  nwsPrototype1
+//
+//  Created by Mauricio Sanchez Duque on 10/5/13.
+//
+//
+
+
+#pragma once
+#include "ofMain.h"
+
+class Dancer {
+    
+public:
+    
+    
+    Dancer();
+    
+    void draw();
+    
+    void update();
+    
+    
+    
+    
+    
+private:
+    
+    
+    
+};
