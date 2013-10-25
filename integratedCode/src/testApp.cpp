3dd62156b662b1925bad3b2369c27f1d467d4dfd
J. Matthew Griffis <grifj153@newschool.edu>
2013-10-25 17:23:30 -0400

    Finished adding Lines to the integrated project. Now for Mauricio's sketch.

--- integratedCode/src/testApp.cpp
+++ integratedCode/src/testApp.cpp
@@ -3,7 +3,8 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 	
-	//setupFFT();
+//	setupFFT();
+    setupLines();
 }
 
 
@@ -11,18 +12,38 @@ void testApp::setup(){
 void testApp::update(){
     
 //    updateFFT();
+    updateLines();
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
     
 //    drawFFT();
+    drawLines();
 }
 
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
 	
+    // Reset everything with 'r' (debug).
+    if ( key == 'r' ) {
+        setup();
+    }
+    
+    //-------------LINES-------------
+    
+    // Toggle "fill in" mode on and off.
+    if ( key == '1' ) {
+        for ( int i = 0; i < dancerList.size(); i++ ){
+            dancerList[ i ].fillIn = !dancerList[ i ].fillIn;
+        }
+    }
+    
+    // Toggle "noiseBreath" mode on and off.
+    if ( key == '2' ) {
+        noiseBreath = !noiseBreath;
+    }
 }
 
 //--------------------------------------------------------------
@@ -43,6 +64,80 @@ void testApp::mousePressed(int x, int y, int button){
 //--------------------------------------------------------------
 void testApp::mouseReleased(){
     
+}
+
+//--------------------------------------------------------------
+void testApp::setupLines() {
+    
+    // First of all, clear out the vectors (in case of reset).
+    // First the lineLists inside the dancerList...
+    for ( int i = 0; i < dancerList.size(); i++) {
+        dancerList[ i ].lineList.clear();
+    }
+    // ...then the dancerList itself.
+    dancerList.clear();
+    
+    // Maintenance
+    ofSetVerticalSync( true );
+    ofSetFrameRate( 60 );
+    ofSetCircleResolution( 100 );
+    
+    // May want to change this to a gradient or something else.
+    ofBackground( 0 );
+    
+    breath = 0;
+    breathRad = 50;
+    noiseBreath = true;
+    
+    // Create Dancers, feed in a pos and vel, and add them to the vector.
+    for ( int i = 0; i < NUMDANCERS; i++ ) {
+        Dancer instrument( ofVec2f( ofRandomWidth(), ofRandomHeight() ),  ofVec2f( ofRandom( -0.5, 0.5 ), ofRandom( -0.5, 0.5 ) ) );
+        dancerList.push_back( instrument );
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::updateLines() {
+    
+    // Set the color back to default.
+    dancerList[ 0 ].c.set( ofColor( 255, 0, 0 ) );
+    dancerList[ 1 ].c.set( ofColor( 0, 255, 0 ) );
+    dancerList[ 2 ].c.set( ofColor( 0, 0, 255 ) );
+    
+    // Change the color upon collision. This isn't working right now and it also makes the app run really slowly, so I'm turning it off.
+    /*for ( int i = 0; i < dancerList.size(); i++ ) {
+     for ( int j = 0; j < dancerList.size(); j++ ) {
+     for ( int k = 0; k < dancerList[ i ].lineList.size(); k++ ) {
+     for ( int l = 0; l < dancerList[ j ].lineList.size(); l++ ) {
+     if ( dancerList[ i ].lineList[ k ].pos == dancerList[ j ].lineList[ l ].pos ) {
+     dancerList[ i ].lineList[ k ].c = ofColor(255);
+     dancerList[ j ].lineList[ l ].c = ofColor(255);
+     }
+     }
+     }
+     }
+     }*/
+    
+    // When we have real breathing data we won't need this, but for the time being, let's fake it.
+    float waveSpeed = 1;
+    if ( !noiseBreath ) {
+        breath = sin( ofGetElapsedTimef() * waveSpeed ) * breathRad;
+    } else {
+        breath = ofNoise( sin( ofGetElapsedTimef() * waveSpeed ) ) * breathRad;
+    }
+    
+    // Pass in the data and update the Dancers.
+    for ( int i = 0; i < dancerList.size(); i++ ) {
+        dancerList[ i ].update( breath );
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::drawLines() {
+    
+    for ( int i = 0; i < dancerList.size(); i++ ) {
+        dancerList[ i ].draw();
+    }
 }
 
 //--------------------------------------------------------------
