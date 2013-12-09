f031695240ebc8cb0406e42cd48d469367859fde
sheva29 <design@sanchezmauricio.com>
2013-12-09 14:14:55 -0500

    Fixed the head bug

--- Mesh/src/main.cpp
+++ Mesh/src/main.cpp
@@ -5,11 +5,9 @@
 //========================================================================
 int main( ){
     ofAppGlutWindow window;
-<<<<<<< HEAD
+
 	ofSetupOpenGL(&window,1920, 1080,OF_WINDOW);			// <-------- setup the GL context
-=======
-	ofSetupOpenGL(&window, 1920,1080,OF_WINDOW);			// <-------- setup the GL context
->>>>>>> 141cf61acd2246c8c47145334110642a567d13e7
+
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
